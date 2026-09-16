e876d0297a3ba0e05eed9639f35bcea99b51d4c9
Arpit <arpit@Arpits-Mac-mini.local>
2026-09-16 18:59:21 +0530

    fixes

--- RRFusion.py
+++ RRFusion.py
@@ -58,8 +58,22 @@ vector_search = [
 scores = {}
 k = 60
 
-for 
+for rank, result in enumerate(bm_results, start=1):
+    scores[result['id']] = scores.get(result['id'], 0)
+    """OR (ABOVE LINE)
+    if doc_id not in rrf_scores:
+        rrf_scores[doc_id] = 0
+    """
+    scores[result['id']] += 1/(k+rank)
 
-"""
-rrf = sum 1/(k+rank)
-"""
+for rank, result in enumerate(vector_search, start=1):
+    scores[result['id']] = scores.get(result['id'], 0)
+    scores[result['id']] += 1/(k+rank)
+print(scores)
+ranked = sorted(
+    scores.items(),
+    key = lambda x:x[1],
+    reverse=True
+)
+print(ranked)
+
